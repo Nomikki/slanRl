22603272d43eaa491d712404c6bcedc082bf8484
Kimmo Saari <kirbownz@me.com>
2021-10-18 22:15:06 +0300

    Read `version` from `package.json`

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { version } = require("./package.json");
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,7 +15,7 @@ module.exports = {
     new webpack.DefinePlugin({
       PRODUCTION: JSON.stringify(true),
       COMMIT_HASH: JSON.stringify(process.env.COMMIT_HASH || "dev"),
-      VERSION: JSON.stringify(process.env.VERSION || "dev"),
+      VERSION: JSON.stringify(process.env.VERSION || version || "dev"),
     }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
