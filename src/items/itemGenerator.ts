ced93feef3aab470585417f3bc372896a66c6285
Kimon Kukulis <kimon.kukulis@gmail.com>
2021-10-31 22:36:15 +0200

    All items is moved to json files

--- src/items/itemGenerator.ts
+++ src/items/itemGenerator.ts
@@ -1,8 +1,12 @@
+import armorsJson from "../data/armors.json";
+import itemsJson from "../data/items.json";
+import weaponsJson from "../data/weapons.json";
 import Armor, { ArmorType } from "../rpg/armor";
 import Weapon, { DamageType } from "../rpg/weapon";
 import Actor from "../units/actor";
 import { ConfusedMonsterAi } from "../units/ai";
 import { Colors } from "../utils/colors";
+import { ensure } from "../utils/utils";
 import Pickable, {
   AiChangeEffect,
   HealthEffect,
@@ -12,6 +16,58 @@ import Pickable, {
   Wearable,
   WearableType,
 } from "./pickable";
+
+interface ArmorInterface {
+  name: string;
+  ac: number;
+  armorType: string;
+  weight: number;
+}
+
+interface WeaponInterface {
+  name: string;
+  damage: string;
+  damageType: string;
+  weight: number;
+  wearableType: string;
+}
+
+interface Selector {
+  type: string;
+  range: number;
+}
+
+interface Effect {
+  type: string;
+  amount: number;
+}
+
+interface ItemInterface {
+  name: string;
+  message: string;
+  ch: string;
+  color: string;
+  weight: number;
+
+  selector: Selector;
+  effect: Effect;
+}
+
+export const armors: ArmorInterface[] = armorsJson;
+export const weapons: WeaponInterface[] = weaponsJson;
+export const items: ItemInterface[] = itemsJson;
+
+const getArmorUsingFind = (name: string): ArmorInterface | undefined => {
+  return armors.find(item => item.name === name);
+};
+
+const getWeaponsUsingFind = (name: string): WeaponInterface | undefined => {
+  return weapons.find(item => item.name === name);
+};
+
+const getItemsUsingFind = (name: string): ItemInterface | undefined => {
+  return items.find(item => item.name === name);
+};
 
 export const createItem = (props: {
   name: string;
@@ -26,146 +82,79 @@ export const createItem = (props: {
   let armor = undefined;
   let weapon = undefined;
 
+  let armorType = ArmorType.LIGHT_ARMOR;
+
+  const isArmor = (name: string): boolean => {
+    for (const n of armors) {
+      if (n.name === name) return true;
+    }
+    return false;
+  };
+
+  const isWeapon = (name: string): boolean => {
+    for (const n of weapons) {
+      if (n.name === name) return true;
+    }
+    return false;
+  };
+
+  const isItem = (name: string): boolean => {
+    for (const n of items) {
+      if (n.name === name) return true;
+    }
+    return false;
+  };
+
   //simple melee weapons
-  if (props.name === "club") {
+  if (isWeapon(props.name)) {
+    const weaponTemplate = ensure(getWeaponsUsingFind(props.name));
+    let damageType = DamageType.BLUDGEONING;
+    let wearableType = WearableType.ONEHANDED_WEAPON;
+
+    if (weaponTemplate.damageType === "bludgeoning")
+      damageType = DamageType.BLUDGEONING;
+    if (weaponTemplate.damageType === "slashing")
+      damageType = DamageType.SLASHING;
+    if (weaponTemplate.damageType === "piercing")
+      damageType = DamageType.PIERCING;
+
+    if (weaponTemplate.wearableType === "one-handed")
+      wearableType = WearableType.ONEHANDED_WEAPON;
+    if (weaponTemplate.wearableType === "two-handed")
+      wearableType = WearableType.TWOHANDED_WEAPON;
+
     weapon = new Weapon({
       name: props.name,
-      damage: "1d4",
-      damageType: DamageType.BLUDGEONING,
+      damage: weaponTemplate?.damage,
+      damageType: damageType,
     });
     ch = "F";
     color = Colors.WEAPON_ITEM;
     blocks = false;
     pickable = new Pickable({
       selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 2,
-    });
-  } else if (props.name === "dagger") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d4",
-      damageType: DamageType.PIERCING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 1,
-    });
-  } else if (props.name === "greatclub") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d8",
-      damageType: DamageType.BLUDGEONING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.TWOHANDED_WEAPON),
-      weight: 10,
-    });
-  } else if (props.name === "handaxe") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d6",
-      damageType: DamageType.SLASHING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 2,
-    });
-  } else if (props.name === "javelin") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d6",
-      damageType: DamageType.PIERCING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 2,
-    });
-  } else if (props.name === "light hammer") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d4",
-      damageType: DamageType.BLUDGEONING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 2,
-    });
-  } else if (props.name === "mace") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d6",
-      damageType: DamageType.BLUDGEONING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 4,
-    });
-  } else if (props.name === "quarterstaff") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d6",
-      damageType: DamageType.BLUDGEONING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 4,
-    });
-  } else if (props.name === "spear") {
-    weapon = new Weapon({
-      name: props.name,
-      damage: "1d6",
-      damageType: DamageType.PIERCING,
-    });
-    ch = "F";
-    color = Colors.WEAPON_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ONEHANDED_WEAPON),
-      weight: 3,
+      effect: new Wearable(wearableType),
+      weight: weaponTemplate.weight,
     });
   }
-
   //simple ranged weapons
 
   //armors
+  else if (isArmor(props.name)) {
+    const armorTemplate = ensure(getArmorUsingFind(props.name));
 
-  //light armors
-  else if (props.name === "leather armor") {
+    if (armorTemplate.armorType === "light") armorType = ArmorType.LIGHT_ARMOR;
+    if (armorTemplate.armorType === "medium")
+      armorType = ArmorType.MEDIUM_ARMOR;
+    if (armorTemplate.armorType === "heavy") armorType = ArmorType.HEAVY_ARMOR;
+    if (armorTemplate.armorType === "shield") armorType = ArmorType.SHIELD;
+
     armor = new Armor({
       name: props.name,
-      ac: 11,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.LIGHT_ARMOR,
+      ac: armorTemplate.ac,
+      armorClassAbilityType:
+        armorType === ArmorType.HEAVY_ARMOR ? "str" : "dex",
+      armorType: armorType,
       requirementStrenght: 0,
     });
     ch = "L";
@@ -173,243 +162,56 @@ export const createItem = (props: {
     blocks = false;
     pickable = new Pickable({
       selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 10,
+      effect: new Wearable(
+        armorType === ArmorType.SHIELD
+          ? WearableType.SHIELD
+          : WearableType.ARMOR,
+      ),
+      weight: armorTemplate.weight,
     });
-  } else if (props.name === "studded leather") {
-    armor = new Armor({
-      name: props.name,
-      ac: 12,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.LIGHT_ARMOR,
-      requirementStrenght: 0,
-    });
-    ch = "L";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 13,
-    });
-  } else if (props.name === "hide") {
-    armor = new Armor({
-      name: props.name,
-      ac: 12,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.MEDIUM_ARMOR,
-      requirementStrenght: 2,
-    });
-    ch = "M";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 6,
-    });
-  } else if (props.name === "chain shirt") {
-    armor = new Armor({
-      name: props.name,
-      ac: 13,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.MEDIUM_ARMOR,
-      weight: 20,
-      requirementStrenght: 0,
-    });
-    ch = "M";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 20,
-    });
-  } else if (props.name === "scale mail") {
-    armor = new Armor({
-      name: props.name,
-      ac: 14,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.MEDIUM_ARMOR,
-      weight: 4,
-      requirementStrenght: 5,
-    });
-    ch = "M";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 45,
-    });
-  } else if (props.name === "breastplate") {
-    armor = new Armor({
-      name: props.name,
-      ac: 14,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.MEDIUM_ARMOR,
-      requirementStrenght: 0,
-    });
-    ch = "M";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 20,
-    });
-  } else if (props.name === "half plate") {
-    armor = new Armor({
-      name: props.name,
-      ac: 15,
-      armorClassAbilityType: "dex",
-      armorType: ArmorType.MEDIUM_ARMOR,
-      requirementStrenght: 0,
-    });
-    ch = "M";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 40,
-    });
-  } else if (props.name === "ring mail") {
-    armor = new Armor({
-      name: props.name,
-      ac: 14,
-      armorClassAbilityType: "str",
-      armorType: ArmorType.HEAVY_ARMOR,
-      requirementStrenght: 0,
-    });
-    ch = "H";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 40,
-    });
-  } else if (props.name === "chain mail") {
-    armor = new Armor({
-      name: props.name,
-      ac: 16,
-      armorClassAbilityType: "str",
-      armorType: ArmorType.HEAVY_ARMOR,
-      requirementStrenght: 13,
-    });
-    ch = "H";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 55,
-    });
-  } else if (props.name === "splint") {
-    armor = new Armor({
-      name: props.name,
-      ac: 17,
-      armorClassAbilityType: "str",
-      armorType: ArmorType.HEAVY_ARMOR,
-      requirementStrenght: 15,
-    });
-    ch = "H";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 60,
-    });
-  } else if (props.name === "plate") {
-    armor = new Armor({
-      name: props.name,
-      ac: 18,
-      armorClassAbilityType: "str",
-      armorType: ArmorType.HEAVY_ARMOR,
-      requirementStrenght: 15,
-    });
-    ch = "H";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.ARMOR),
-      weight: 65,
-    });
-  } else if (props.name === "shield") {
-    armor = new Armor({
-      name: props.name,
-      ac: 2,
-      armorClassAbilityType: "",
-      armorType: ArmorType.SHIELD,
-      requirementStrenght: 0,
-    });
-    ch = "H";
-    color = Colors.ARMOR_ITEM;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new Wearable(WearableType.SHIELD),
-      weight: 6,
-    });
-  } else if (props.name === "health potion") {
-    ch = "!";
-    color = Colors.HEALTHPOTION;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new HealthEffect(4, undefined),
-      weight: 0.1,
-    });
-  } else if (props.name === "nutella bun") {
-    ch = "@";
-    color = Colors.HEALTHPOTION;
-    blocks = false;
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new HealthEffect(30, undefined),
-      weight: 0.1,
-    });
-  } else if (props.name === "scroll of lighting bolt") {
-    ch = "#";
-    color = Colors.SCROLL_OF_LIGHTING;
-    blocks = false;
-    pickable = pickable = new Pickable({
-      selector: new TargetSelector(SelectorType.CLOSEST_MONSTER, 5),
-      effect: new HealthEffect(-20, "A lighting bolt strikes!"),
-      weight: 0.05,
-    });
-  } else if (props.name === "scroll of fireball") {
-    ch = "#";
-    color = Colors.SCROLL_OF_FIREBALL;
+  } else if (isItem(props.name)) {
+    const itemTemplate = ensure(getItemsUsingFind(props.name));
+    ch = itemTemplate.ch;
+    color = itemTemplate.color;
+
     blocks = false;
 
-    pickable = new Pickable({
-      selector: new TargetSelector(SelectorType.SELECTED_RANGE, 3),
-      effect: new HealthEffect(-12, "hurdur"),
-      weight: 0.05,
-    });
-  } else if (props.name === "scroll of confusion") {
-    ch = "#";
-    color = Colors.SCROLL_OF_CONFUSION;
-    blocks = false;
+    let selector = undefined;
+    let effect = undefined;
+
+    if (itemTemplate.selector.type === "selected range")
+      selector = new TargetSelector(
+        SelectorType.SELECTED_RANGE,
+        itemTemplate.selector.range,
+      );
+    if (itemTemplate.selector.type === "selected monster")
+      selector = new TargetSelector(
+        SelectorType.SELECTED_MONSTER,
+        itemTemplate.selector.range,
+      );
+    if (itemTemplate.selector.type === "closest monster")
+      selector = new TargetSelector(
+        SelectorType.CLOSEST_MONSTER,
+        itemTemplate.selector.range,
+      );
+
+    if (itemTemplate.effect.type === "health")
+      effect = new HealthEffect(
+        itemTemplate.effect.amount,
+        itemTemplate.message,
+      );
+    if (itemTemplate.effect.type === "map clear")
+      effect = new MapClearEffect(itemTemplate.message);
+    if (itemTemplate.effect.type === "change ai")
+      effect = new AiChangeEffect(
+        new ConfusedMonsterAi(itemTemplate.effect.amount),
+        itemTemplate.message,
+      );
 
     pickable = new Pickable({
-      selector: new TargetSelector(SelectorType.SELECTED_MONSTER, 5),
-      effect: new AiChangeEffect(new ConfusedMonsterAi(10), "confused af"),
-      weight: 0.05,
-    });
-  } else if (props.name === "scroll of map") {
-    ch = "#";
-    color = Colors.SCROLL_OF_MAP;
-    blocks = false;
-
-    pickable = new Pickable({
-      selector: undefined,
-      effect: new MapClearEffect("All is clear!"),
-      weight: 0.05,
+      selector: selector,
+      effect: effect,
+      weight: itemTemplate.weight,
     });
   } else {
     console.error(`${name} not found.`);
