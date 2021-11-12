24246a49bfed116d74dcec173ff8736a1e0d927a
Kimmo Saari <kirbownz@me.com>
2021-11-12 13:27:53 +0200

    Playwright improvements

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,18 +1,25 @@
 import { expect, test } from "@playwright/test";
 import { config } from "../playwright.config";
 
+const testStep = async (
+  title: string,
+  body: ({ title }: { title: string }) => Promise<unknown>,
+) => test.step(title, () => body({ title }));
+
 test("run tests", async ({ page, browserName, viewport }) => {
   await page.goto(`http://localhost:${config?.webServer?.port || 8080}`);
   const pageTitle = page.locator("title");
   await expect(pageTitle).toHaveText("Slan Roguelike");
 
-  const { width, height } = viewport || {};
+  test.slow();
 
-  await test.step("initial view snapshot", async () => {
-    await page.screenshot({
-      path: `test-results/snapshots/${browserName}/test1_${width}x${height}.png`,
-      fullPage: true,
-    });
+  const screenshotSettings = (title: string) => ({
+    path: `test-results/snapshots/${browserName}/${title}_${viewport?.width}x${viewport?.height}.png`,
+    fullPage: true,
+  });
+
+  await testStep("initial view snapshot", async ({ title }) => {
+    await page.screenshot(screenshotSettings(title));
 
     // expect(
     //   await page.screenshot({
@@ -21,7 +28,7 @@ test("run tests", async ({ page, browserName, viewport }) => {
     // ).toMatchSnapshot(`tests/screenshots/${browserName}-screenshot.png`);
   });
 
-  await test.step("character build menu", async () => {
+  await testStep("character build menu", async ({ title }) => {
     await page.keyboard.press("Enter");
 
     await page.keyboard.press("ArrowDown");
@@ -48,10 +55,7 @@ test("run tests", async ({ page, browserName, viewport }) => {
     await page.keyboard.press("ArrowRight");
     await page.keyboard.press("ArrowRight");
 
-    await page.screenshot({
-      path: `test-results/snapshots/${browserName}/test2_${width}x${height}.png`,
-      fullPage: true,
-    });
+    await page.screenshot(screenshotSettings(title));
 
     // expect(
     //   await page.screenshot({
@@ -60,13 +64,10 @@ test("run tests", async ({ page, browserName, viewport }) => {
     // ).toMatchSnapshot(`tests/screenshots/${browserName}-screenshot.png`);
   });
 
-  await test.step("start new game", async () => {
+  await testStep("start new game", async ({ title }) => {
     await page.keyboard.press("Enter");
 
-    await page.screenshot({
-      path: `test-results/snapshots/${browserName}/test3_${width}x${height}.png`,
-      fullPage: true,
-    });
+    await page.screenshot(screenshotSettings(title));
 
     // expect(
     //   await page.screenshot({
--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,6 +1,27 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
+type BrowserName = "chromium" | "firefox" | "webkit";
+
 const port = 8081;
+
+const resolutions = [
+  // { width: 768, height: 576 },
+  { width: 1280, height: 720 },
+  { width: 1920, height: 1200 },
+  // { width: 3840, height: 2160 },
+];
+
+const browserConfig = (browserName: BrowserName) => {
+  const configs = resolutions.map(({ width, height }) => ({
+    name: `${browserName}-${width}-${height}`,
+    use: {
+      browserName,
+      viewport: { width, height },
+    },
+  }));
+
+  return configs;
+};
 
 export const config: PlaywrightTestConfig = {
   timeout: 120 * 1000,
@@ -16,23 +37,7 @@ export const config: PlaywrightTestConfig = {
       slowMo: process.env.SLOW ? 250 : 0,
     },
   },
-  projects: [
-    {
-      name: "Chromium-1280-720",
-      use: {
-        browserName: "chromium",
-        viewport: { width: 1280, height: 720 },
-      },
-    },
-
-    {
-      name: "Firefox-1280-720",
-      use: {
-        browserName: "firefox",
-        viewport: { width: 1280, height: 720 },
-      },
-    },
-  ],
+  projects: [...browserConfig("chromium"), ...browserConfig("firefox")],
 
   webServer: {
     command: `cross-env PORT=${port} yarn webpack:start`,
